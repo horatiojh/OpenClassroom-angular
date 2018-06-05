026e3d1cf13f9949f980d8f75891afd56f0fc2e4
Yongxue <yongxue0701@gmail.com>
2018-06-05 14:35:47 +0800

    view request courses

--- src/app/pages/prof_view_request_course/prof_view_request_course.component.ts
+++ src/app/pages/prof_view_request_course/prof_view_request_course.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { ShareService } from "../../../providers/shareService";
+import { CourseService } from "../../../providers/courseService";
+
+import { Course } from "../../../domain/course";
+
+import { Message } from "primeng/primeng";
 
 @Component({
   selector: "app-profViewRequestCourse",
@@ -7,19 +12,45 @@ import { ShareService } from "../../../providers/shareService";
   styleUrls: ["./prof_view_request_course.component.css"]
 })
 export class ProfViewRequestCourseComponent implements OnInit {
+  // attributes
   weekDay: string;
   startTime: string;
   endTime: string;
 
-  constructor(private shareService: ShareService) {}
+  // for datatable
+  courses: Course[];
+
+  // for component
+  msgs: Message[] = [];
+
+  constructor(
+    private shareService: ShareService,
+    private courseService: CourseService
+  ) {}
 
   ngOnInit() {
     this.weekDay = this.shareService.getValue("weekDay");
     this.startTime = this.shareService.getValue("startTime");
     this.endTime = this.shareService.getValue("endTime");
 
-    console.log(this.weekDay);
-    console.log(this.startTime);
-    console.log(this.endTime);
+    let endpoint = "/getRequestCourses";
+    let body = {
+      weekDay: this.weekDay,
+      startTime: this.startTime,
+      endTime: this.endTime
+    };
+
+    this.courseService.getRequestCourses(endpoint, body).subscribe(response => {
+      if (response != null && typeof response.courses != undefined) {
+        this.courses = response.courses;
+        console.log(this.courses);
+      } else {
+        this.msgs.push({
+          severity: "error",
+          summary: "An error has occurred while processing the request",
+          detail: ""
+        });
+      }
+    });
   }
 }
--- src/providers/courseService.ts
+++ src/providers/courseService.ts
@@ -1,8 +1,12 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
 import { Observable } from "rxjs/Observable";
-import 'rxjs/add/observable/throw';
+import "rxjs/add/observable/throw";
 import { of } from "rxjs";
 import { Course } from "../domain/course";
 
@@ -12,54 +16,67 @@ const httpOptions = {
 
 @Injectable()
 export class CourseService {
+  baseUrl = "/api/course";
 
-    baseUrl = "/api/course";
+  constructor(private httpClient: HttpClient) {}
 
-    constructor(private httpClient: HttpClient) {
+  getAllCourses(): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + "/getAllRecords");
+  }
+
+  getCoursesByStaffId(staffId: number): Observable<any> {
+    return this.httpClient
+      .get<any>(this.baseUrl + "/getCourses/" + staffId)
+      .pipe(
+        tap(_ => console.log(`getCourses staffId=${staffId}`)),
+        catchError(this.handleError<any>(`getCourses staffId=${staffId}`))
+      );
+  }
+
+  getCourseByCourseId(courseId: number): Observable<any> {
+    return this.httpClient
+      .get<any>(this.baseUrl + "/getCourse/" + courseId)
+      .pipe(
+        tap(_ => console.log(`getCourse timetableId=${courseId}`)),
+        catchError(this.handleError<any>(`getCourse timetableId=${courseId}`))
+      );
+  }
+
+  updateCourse(course: Course): Observable<any> {
+    let updateCourseReq = { course: course };
+
+    return this.httpClient
+      .post<any>(this.baseUrl + "/updateCourse", updateCourseReq, httpOptions)
+      .pipe(
+        tap(_ => console.log(`updateCourse id=${course.id}`)),
+        catchError(this.handleError<any>(`updateCourse id=${course.id}`))
+      );
+  }
+
+  getRequestCourses(endpoint: string, body?: any): Observable<any> {
+    return this.httpClient
+      .post<any>(this.baseUrl + endpoint, body)
+      .pipe(tap(resp => console.log(resp)), catchError(this.handleErrorApi));
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      return of(result as T);
+    };
+  }
+
+  private handleErrorApi(error: HttpErrorResponse) {
+    let errMsg = error.message || "Server error";
+
+    if (error.error instanceof ErrorEvent) {
+      console.error("An unknown error has occurred:", error.error.message);
+    } else {
+      console.error(
+        "An HTTP error has occurred: " +
+          `HTTP ${error.status}: ${error.error.message}`
+      );
     }
 
-    getAllCourses(): Observable<any> {
-        return this.httpClient.get<any>(this.baseUrl + "/getAllRecords");
-    }
-
-    getCoursesByStaffId(staffId: number): Observable<any> {
-      return this.httpClient
-        .get<any>(this.baseUrl + "/getCourses/" + staffId)
-        .pipe(
-          tap(_ => console.log(`getCourses staffId=${staffId}`)),
-          catchError(this.handleError<any>(`getCourses staffId=${staffId}`))
-        );
-    }
-
-    getCourseByCourseId(courseId: number): Observable<any> {
-      return this.httpClient
-        .get<any>(this.baseUrl + "/getCourse/" + courseId)
-        .pipe(
-          tap(_ => console.log(`getCourse timetableId=${courseId}`)),
-          catchError(
-            this.handleError<any>(`getCourse timetableId=${courseId}`)
-          )
-        );
-    }
-
-    updateCourse(course: Course): Observable<any> {
-      let updateCourseReq = { "course": course };
-
-      return this.httpClient
-        .post<any>(
-          this.baseUrl + "/updateCourse",
-          updateCourseReq,
-          httpOptions
-        )
-        .pipe(
-          tap(_ => console.log(`updateCourse id=${course.id}`)),
-          catchError(this.handleError<any>(`updateCourse id=${course.id}`))
-        );
-    }
-
-    private handleError<T>(operation = "operation", result?: T) {
-      return (error: any): Observable<T> => {
-        return of(result as T);
-      };
-    }
+    return Observable.throw(errMsg);
+  }
 }
