03ceb898a760ea5631f2a121a54e6be4b5f181f7
Yongxue <yongxue0701@gmail.com>
2018-07-05 22:35:51 +0800

    angular - rating app

--- src/app/pages/admin/app_rating/app_rating.component.ts
+++ src/app/pages/admin/app_rating/app_rating.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SafeStyle, DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-appRating',
-  templateUrl: './app_rating.component.html',
-  styleUrls: ['./app_rating.component.css']
+  selector: "app-appRating",
+  templateUrl: "./app_rating.component.html",
+  styleUrls: ["./app_rating.component.css"]
 })
 export class AppRatingComponent implements OnInit {
+  title: string;
+  rating: number;
+  comments: string;
 
-  constructor() { }
+  // for css
+  submitRatingBtnStyle: SafeStyle;
+
+  constructor(private domSanitizer: DomSanitizer) {}
 
   ngOnInit() {
+    // for css
+    let submitRatingStaffStyle = "width:180%;height:34px";
+    this.submitRatingBtnStyle = this.domSanitizer.bypassSecurityTrustStyle(
+      submitRatingStaffStyle
+    );
   }
 
+  submitRating() {}
 }
