5971e45099ae8f2ef85bdc4586dd4badc217e2a4
Yongxue <yongxue0701@gmail.com>
2018-09-18 23:04:06 +0800

    update - request class visit (sort visitors' name)

--- src/app/pages/card/request_date_card/request_date_card.component.ts
+++ src/app/pages/card/request_date_card/request_date_card.component.ts
@@ -77,6 +77,13 @@ export class RequestDateCardComponent implements OnInit {
     // for request classroom visit
     this.staffService.getAllInstructors().subscribe(response => {
       this.staffs = response.staffs;
+
+      this.staffs = this.staffs.sort((a, b) => {
+        var textA = a.staffName[0].toUpperCase();
+        var textB = b.staffName[0].toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
+
       this.staffItems = [{ label: "Please Select One", value: null }];
 
       for (let i = 0; i < this.staffs.length; i++) {
