7dfde021733172eedf0f959e49f738b1e0ec0c85
Yongxue <yongxue0701@gmail.com>
2019-02-17 23:20:22 +0800

    top bar

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -12,71 +12,101 @@ import { Message } from "primeng/api";
 @Component({
   selector: "app-topbar",
   template: `
-        <p-growl [(value)]="notificationMsgs" sticky="true" (onClick)="growlOnClick($event)"></p-growl>
+    <p-growl
+      [(value)]="notificationMsgs"
+      sticky="true"
+      (onClick)="growlOnClick($event)"
+    ></p-growl>
 
-        <div class="topbar clearfix">
-            <div class="topbar-left">
-                <div style="font-size: 24px;color: aliceblue;margin-top: 7px">Open Classroom</div>
-            </div>
-
-            <div class="topbar-right">
-                <a id="menu-button" href="#" (click)="app.onMenuButtonClick($event)">
-                    <i></i>
-                </a>
-
-                <a id="topbar-menu-button" href="#" (click)="app.onTopbarMenuButtonClick($event)">
-                    <i class="material-icons">menu</i>
-                </a>
-
-                <ul class="topbar-items animated fadeInDown" [ngClass]="{'topbar-items-visible': app.topbarMenuActive}">
-                    <li #profile class="profile-item" *ngIf="app.profileMode==='top'||app.isHorizontal()"
-                        [ngClass]="{'active-top-menu':app.activeTopbarItem === profile}">
-
-                        <a href="#" (click)="app.onTopbarItemClick($event,profile)">
-                            <img class="profile-image" src="assets/layout/images/avatar.png" />
-                            <span class="topbar-item-name">Jane Williams</span>
-                        </a>
-
-                        <ul class="ultima-menu animated fadeInDown">
-                            <li role="menuitem">
-                                <a href="#">
-                                    <i class="material-icons">person</i>
-                                    <span>Profile</span>
-                                </a>
-                            </li>
-                            <li role="menuitem">
-                                <a href="#">
-                                    <i class="material-icons">star_rate</i>
-                                    <span>Rate App</span>
-                                </a>
-                            </li>
-                            <li role="menuitem">
-                                <a href="#">
-                                    <i class="material-icons">power_settings_new</i>
-                                    <span>Logout</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li #notifications [ngClass]="{'active-top-menu':app.activeTopbarItem === notifications}"
-                    [style]="notificationStyle">
-                        <a routerLink="/profViewNotification">
-                            <i class="topbar-icon material-icons">timer</i>
-                            <span class="topbar-badge animated rubberBand">{{numOfNewMsg}}</span>
-                            <span class="topbar-item-name">Notifications</span>
-                        </a>
-                    </li>
-
-                    <li #search class="search-item" [ngClass]="{'active-top-menu':app.activeTopbarItem === search}">
-                        <a [routerLink]="searchLink">
-                            <i class="topbar-icon material-icons">search</i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+    <div class="topbar clearfix">
+      <div class="topbar-left">
+        <div style="font-size: 24px;color: aliceblue;margin-top: 7px">
+          Open Classroom
         </div>
-    `
+      </div>
+
+      <div class="topbar-right">
+        <a id="menu-button" href="#" (click)="app.onMenuButtonClick($event)">
+          <i></i>
+        </a>
+
+        <a
+          id="topbar-menu-button"
+          href="#"
+          (click)="app.onTopbarMenuButtonClick($event)"
+        >
+          <i class="material-icons">menu</i>
+        </a>
+
+        <ul
+          class="topbar-items animated fadeInDown"
+          [ngClass]="{ 'topbar-items-visible': app.topbarMenuActive }"
+        >
+          <li
+            #profile
+            class="profile-item"
+            *ngIf="app.profileMode === 'top' || app.isHorizontal()"
+            [ngClass]="{ 'active-top-menu': app.activeTopbarItem === profile }"
+          >
+            <a href="#" (click)="app.onTopbarItemClick($event, profile)">
+              <img
+                class="profile-image"
+                src="assets/layout/images/avatar.png"
+              />
+              <span class="topbar-item-name">Jane Williams</span>
+            </a>
+
+            <ul class="ultima-menu animated fadeInDown">
+              <li role="menuitem">
+                <a href="#">
+                  <i class="material-icons">person</i>
+                  <span>Profile</span>
+                </a>
+              </li>
+              <li role="menuitem">
+                <a href="#">
+                  <i class="material-icons">star_rate</i>
+                  <span>Rate App</span>
+                </a>
+              </li>
+              <li role="menuitem">
+                <a href="#">
+                  <i class="material-icons">power_settings_new</i>
+                  <span>Logout</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li
+            #notifications
+            [ngClass]="{
+              'active-top-menu': app.activeTopbarItem === notifications
+            }"
+            [style]="notificationStyle"
+          >
+            <a routerLink="/profViewNotification">
+              <i class="topbar-icon material-icons">timer</i>
+              <span class="topbar-badge animated rubberBand">{{
+                numOfNewMsg
+              }}</span>
+              <span class="topbar-item-name">Notifications</span>
+            </a>
+          </li>
+
+          <li
+            #search
+            class="search-item"
+            [ngClass]="{ 'active-top-menu': app.activeTopbarItem === search }"
+          >
+            <a [routerLink]="searchLink">
+              <i class="topbar-icon material-icons">search</i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `
 })
 export class AppTopbarComponent implements OnInit {
   searchLink: string;
@@ -133,28 +163,28 @@ export class AppTopbarComponent implements OnInit {
         });
 
       // this.interval = setInterval(() => {
-        this.msgService
-          .getUnreadMessagesByStaffId(this.staffId)
-          .subscribe(response => {
-            this.newMsgs = response.messages;
-            this.numOfNewMsg = this.newMsgs.length;
+      this.msgService
+        .getUnreadMessagesByStaffId(this.staffId)
+        .subscribe(response => {
+          this.newMsgs = response.messages;
+          this.numOfNewMsg = this.newMsgs.length;
 
-            if (this.numOfNewMsg === 1) {
-              this.notificationMsgs.push({
-                severity: "warn",
-                summary: "You have a new message",
-                detail: ""
-              });
-              // clearInterval(this.interval);
-            } else if (this.numOfNewMsg > 1) {
-              this.notificationMsgs.push({
-                severity: "warn",
-                summary: "You have new messages",
-                detail: ""
-              });
-              // clearInterval(this.interval);
-            }
-          });
+          if (this.numOfNewMsg === 1) {
+            this.notificationMsgs.push({
+              severity: "warn",
+              summary: "You have a new message",
+              detail: ""
+            });
+            // clearInterval(this.interval);
+          } else if (this.numOfNewMsg > 1) {
+            this.notificationMsgs.push({
+              severity: "warn",
+              summary: "You have new messages",
+              detail: ""
+            });
+            // clearInterval(this.interval);
+          }
+        });
       // }, 1000);
     }
   }
